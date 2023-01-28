a0d5b7899b54f487a40eeb2bb48bfc02fa38c12a
lelygaga <leslieastt@gmail.com>
2023-01-28 14:29:12 -0600

    started learning javascript

---
+++ js/first-js.js
@@ -0,0 +1,22 @@
+// This is a single line comment!
+/*
+This is 
+a multiline 
+comment.*/
+// PLain english comment
+
+// console.log("hello world!");
+// DAta Types 
+// console.log('Single Quote String!');
+// console.log('I can\'t stop thinking about code');
+// console.log('I am yelling at you'.toUpperCase())
+// console.log('I aM YeLlInG aT yOu!'.toLowerCase())
+// console.log('this is a string        '.length)
+
+let myFirstVariable
+myFirstVariable = "Hi from a Variable";
+console.log(myFirstVariable);
+
+let kali = "kali"
+// console.log("hello" + "world!");
+console.log(myFirstVariable + kali);
